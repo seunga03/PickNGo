6c220687021ee3ae9c169770d1af5f70c34aacd6
YongWon Kim <38yong1@gmail.com>
2025-09-30 14:50:41 +0900

    kyw

--- src/com/multi/run/App.java
+++ src/com/multi/run/App.java
@@ -5,8 +5,8 @@ import com.multi.view.MainMenu;
 
 public class App {
     public static void main(String[] args) {
-//        new Intro().intro();
-        new MainMenu().mainMenu();
+       new Intro().intro();
+//        new MainMenu().mainMenu();
     }
     // 용원
     // 수정
